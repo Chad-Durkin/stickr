debb586bb45641b4d1402c947d7fe2d9f5213cbd
Chad Durkin and Sam Kirsch <chad+sam@>
2017-04-27 08:47:24 -0700

    create base files and directories and add the basic models

---
+++ src/Stickr/Models/StickrDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stickr.Models
+{
+    public class StickrDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public StickrDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
